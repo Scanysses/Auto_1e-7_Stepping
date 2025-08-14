e11add0c3355dd38c586a27ae613b1446e2f6180
Scanysses <boralex.borisoff@yandex.ru>
2025-08-14 12:10:29 +0200

    Initial commit

---
+++ src/main/java/net/scanysses/auto_1e7_stepping/Auto_1e7_stepping.java
@@ -0,0 +1,10 @@
+package net.scanysses.auto_1e7_stepping;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Auto_1e7_stepping implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
